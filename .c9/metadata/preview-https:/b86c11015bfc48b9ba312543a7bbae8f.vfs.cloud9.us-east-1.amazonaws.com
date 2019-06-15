93e15904044e679746d115eeeabe72ebba279f97
Ubuntu <ubuntu@ip-172-31-95-64.ec2.internal>
2019-06-15 16:26:12 +0000

    region-selector feature added to the dashboard

---
+++ .c9/metadata/preview-https:/b86c11015bfc48b9ba312543a7bbae8f.vfs.cloud9.us-east-1.amazonaws.com
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://b86c11015bfc48b9ba312543a7bbae8f.vfs.cloud9.us-east-1.amazonaws.com","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://b86c11015bfc48b9ba312543a7bbae8f.vfs.cloud9.us-east-1.amazonaws.com","previewer":"previewBrowser"},"timestamp":1560519852763,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- static/js/graph.js
+++ static/js/graph.js
@@ -2,5 +2,21 @@ queue()
     .defer(d3.csv, "data/Accidents_2017_aggregated.csv")
     .await(makeGraphs);
     
-function makeGraphs(error, data) {
+function makeGraphs(error, accData) {
+    var ndx = crossfilter(accData);
+    
+    show_region_selector(ndx);
+    
+    dc.renderAll();
+}
+
+function show_region_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('region'));
+    var group = dim.group();
+    
+    dc.selectMenu("#region-selector")
+        .dimension(dim)
+        .group(group)
+        .promptText('UK total')
+        .multiple(false);   //change to true if you decide to allow multiple selection
 }
